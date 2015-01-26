be71b2ee773723beed032fed9da03807b769ffb2
Peter Socha <psocha@uwaterloo.ca>
2015-01-26 10:08:53 -0500

    Added printf and debugging functionality

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 #include <LPC17xx.h>
-#include "uart_polling.h"
 #include "rtx.h"
 #include "k_memory.h"
+#include "uart_polling.h"
+
+#ifdef DEBUG_0
+#include "printf.h"
+#endif /* DEBUG_0 */
 
 int main() {
 	
 	SystemInit();
+	
 	uart0_init();
 	uart0_put_string("Hello World!\n\r");
+	
+	#ifdef DEBUG_0
+		init_printf(NULL, uart_put_char);
+		printf("This is working %d", 1);
+	#endif /* DEBUG_0 */
 	
 	/* start the RTX and built-in processes */
 	rtx_init();  
