a5cf6eca6a014c363dc435d63b6309a66f51298e
Mordequess <t.babaran@gmail.com>
2015-01-29 11:05:24 -0500

    memory management overhaul (untested)
    completed release_memory_block, reworked heap initialization and
    request_memory_block

--- k_memory.c
+++ k_memory.c
@@ -4,38 +4,40 @@
 #include "printf.h"
 #endif /* ! DEBUG_0 */
 
-/**
- * @brief: Initialize RAM as follows:
-0x10008000+---------------------------+ High Address
-          |    Proc 1 STACK           |
+/*
+0x10000000+---------------------------+ Low Address
+          |       RTX  Image          |
           |---------------------------|
-          |    Proc 2 STACK           |
-          |---------------------------|<--- gp_stack
-          |                           |
-          |        HEAP               |
-          |                           |
+          |Image$$RW_IRAM1$$ZI$$Limit |
+          |---------------------------|
+          |        Padding            |
+          |---------------------------|<--- gp_pcbs
+          |        PCB pointers       |
+		  |---------------------------|
+          |        PCB 1              |
           |---------------------------|
           |        PCB 2              |
           |---------------------------|
-          |        PCB 1              |
-          |---------------------------|
-          |        PCB pointers       |
-          |---------------------------|<--- gp_pcbs
-          |        Padding            |
-          |---------------------------|  
-          |Image$$RW_IRAM1$$ZI$$Limit |
-          |...........................|          
-          |       RTX  Image          |
+		  |		space for more PCBs	  |
+          |---------------------------|<--- HEAP_START_ADDR
           |                           |
-0x10000000+---------------------------+ Low Address
+          |        HEAP               |
+          |                           |
+          |---------------------------|<--- HEAP_END_ADDR
+          |	   space for more stacks  |
+          |---------------------------|<--- gp_stack 
+          |    Proc 2 STACK           |
+          |...........................|          
+          |    Proc 1 STACK           |
+          |                           |
+0x10008000+---------------------------+ High Address
 */
 
 /* ----- Global Variables ----- */
-heap_blk heap_Head;
 //pcb **gp_pcbs;
-U32 *gp_stack; /* The last allocated stack low address. 8 bytes aligned */
-												/* The first stack starts at the RAM high address */
-												/* stack grows down. Fully decremental stack */
+U32 *gp_stack;	/* The last allocated stack low address. 8 bytes aligned */
+				/* The first stack starts at the RAM high address */
+				/* stack grows down. Fully decremental stack */
 
 /**
  * @brief: allocate stack for a process, align to 8 bytes boundary
@@ -95,66 +97,99 @@ void memory_init(void)
 		--gp_stack; 
 	}
   
-	/* allocate memory for heap, not implemented yet*/
-	
-	//heap fixed start and end address found in memory.h
-	heap_Head.start_Addr = HEAP_START_ADDR;
-	heap_Head.next_Addr = NULL;
-	heap_Head.length = HEAP_START_ADDR - HEAP_END_ADDR;
-	
-	
-	// so at this point, gp_stack is pointing to where
-	// the heap starts - (right?)
-	// and also then, we know that p_end is pointing to where the heap ends
+	//allocate memory for heap
+	heap_blk* heap_Head = (heap_blk*)HEAP_START_ADDR;
+	heap_Head->next_Addr = NULL;
+	heap_Head->length = HEAP_END_ADDR - HEAP_START_ADDR - sizeof(heap_blk *); //length in heap_Head adjusts for header, others won't
 }
-void *k_request_memory_block(void) {
-	/* PROVIDED_PSEUDOCODE
-	atomic ( on ) ;
-	while ( no memory block is available ) {
-		put PCB on blocked_resource_q ;
-		set process state to BLOCKED_ON_RESOURCE ;
-		release_processor ( ) ;
-	}
-	int mem_blk = next free block ;
-	update the heap ;
-	atomic ( off ) ;
-	return mem_blk ;
-	*/
-	void* mem_blk;
-	
-	// BLOCK_SIZE = 0x80 (128)
-	
-	heap_blk* heap = &heap_Head; //start at the top of the heap by making a pointer to heap_Head
-	while (heap->length < BLOCK_SIZE) {
-		if (heap->next_Addr == 0) return NULL; //if this is last free block, the request can't be filled
-		else heap = (heap_blk*)heap->next_Addr; //cast U32 as heap_blk pointer
-	}
-	
-	mem_blk = (void*)(heap->start_Addr + heap->length - BLOCK_SIZE);
-	heap->length -= BLOCK_SIZE;
-	
-	return mem_blk;
-	
-	//case: requested block is entire free space block
-	//		must remove this free space block from linked list (probably. length 0 will not satisfy and requests)
 
+void *k_request_memory_block(void) {
+	//start at the top of the heap by making a pointer to HEAP_START_ADDR
+	heap_blk* temp;
+	heap_blk* freeNode = (heap_blk*)HEAP_START_ADDR;
+	void* mem_blk;
+
+	__disable_irq(); //atomic(on);
+
+	//find the next free space large enough to fulfill the request
+	while (freeNode->length < BLOCK_SIZE) {
+		if (freeNode->next_Addr == 0){ //if this is last free node, the request can't be filled
+			//TODO:
+			//put PCB on blocked_resource_q ;
+			//set process state to BLOCKED_ON_RESOURCE ;
+			//release_processor ( ) ;
+			return NULL; 
+		}
+		else freeNode = (heap_blk*)(freeNode->next_Addr); //cast U32 as a heap_blk pointer to iterate through the linked list
+	}
+	
+	//return the bottom chunk of free space as a memory block, reduce free space
+	mem_blk = (void*)((U32)freeNode + freeNode->length - BLOCK_SIZE);
+	
+	//update free space list
+	freeNode->length -= BLOCK_SIZE;
+	if (freeNode->length == 0 && freeNode != HEAP_START_ADDR) {
+		temp = (heap_blk*)HEAP_START_ADDR;
+		while (freeNode != (heap_blk*)(temp->next_Addr)) {
+			temp = (heap_blk*)(temp->next_Addr); 
+		}
+		temp->next_Addr = freeNode->next_Addr;
+	}
+
+	__enable_irq(); //atomic(off);
+
+	return mem_blk;
 }
 	
 int k_release_memory_block(void *memory_block) {
-	
-	/* PROVIDED PSEUDOCODE
-	atomic ( on ) ;
-	if ( memory block pointer is not valid )
-		return ERROR_CODE ;
-	put memory_block into heap ;
-	if ( blocked on resource q not empty ) {
-		handle_process_ready ( pop ( blocked resource q ) ) ;
+	temp = HEAP_START_ADDR;
+
+	__disable_irq(); //atomic(on);
+
+	//TODO:
+	//if ( memory block pointer is not valid )
+	//	return ERROR_CODE ;
+
+	//special case: memory block is very top of heap
+	if ((U32)memory_block == HEAP_START_ADDR + 4){
+		temp->length += BLOCK_SIZE;
 	}
-	atomic ( off ) ;
-	return SUCCESS_CODE ;
-	*/
-	
-	
+	else {
+		//find where address of mem block lies on free space list
+		while (temp->next_Addr != NULL && temp->next_Addr < (U32)memory_block) {
+			temp = (heap_blk*)temp->next_Addr; 
+		}
+		//if immediately after free space, increase above node
+		if ((U32)memory_block == (U32)temp + temp->length) {
+			temp->length += BLOCK_SIZE;
+		}
+		//else create a node, set "next"
+		else {
+			(heap_blk*)memory_block->length = 0x80;
+			(heap_blk*)memory_block->next_Addr = temp->next_Addr;
+			temp->next_Addr = (U32)memory_block;
+		}
+	}
+	//if a free space node immediately follows, delete that one and steal its "next", increase length
+	if (temp->next_Addr != NULL && temp->next_Addr == (U32)temp + temp->length) {
+		temp->length += (heap_blk*)(temp->next_Addr)->length;
+		temp->next_Addr = (heap_blk*)(temp->next_Addr)->next_Addr;
+	}
+
+
+	//TODO:
+	//if ( "blocked on resource" q not empty ) {
+	//	handle_process_ready ( pop ( blocked resource q ) ) ;
+	//}
+
+	__enable_irq(); //atomic(off);
 	
 	return 0;
 }
+
+
+//release_processor
+//get/set priority
+//priorty queue to hold PCBs
+//blocked "queue", we insert into the middle
+//can be put on the heap
--- k_memory.h
+++ k_memory.h
@@ -7,16 +7,17 @@ extern pcb **gp_pcbs;
 extern U32 Image$$RW_IRAM1$$ZI$$Limit;
 
 #define RAM_END_ADDR 0x10008000
-#define HEAP_START_ADDR 0x10007C00
-#define HEAP_END_ADDR (0x10000000 + 4 * sizeof(pcb *))
+
 
 #define BLOCK_SIZE 0x80
-
 typedef struct heap_blk {
-	U32 start_Addr; //start address of this free space [do we need this?]
 	U32 length;
-	U32 next_Addr; //points to next chunk of free space (used memory will be skipped over)
+	U32 next_Addr;
 } heap_blk;
+
+//heap allocate enough space for X (currently 100) memory blocks + one heap header
+#define HEAP_START_ADDR (0x10000000 + 4 * sizeof(pcb *)) 
+#define HEAP_END_ADDR (HEAP_START_ADDR * 100 + sizeof(heap_blk *))
 
 void memory_init(void);
 U32 *alloc_stack(U32 size_b);
