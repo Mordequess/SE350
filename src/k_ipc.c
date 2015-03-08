56bf3896f4b9a8fedcc7c864a11964543c97335f
Tyler Babaran <tbabaran@uwaterloo.ca>
2015-03-08 10:51:47 -0400

    added message queue
    tyler_messaging

--- src/k_ipc.c
+++ src/k_ipc.c
@@ -1,19 +1,28 @@
 #include "k_ipc.h"
 
-int k_send_message(int process_id, void *message_envelope) {
+/* ----- Global Variables ----- */
+//message* g_message_queue;				/* Message queue */
+
+int k_send_message(int destination_id, void *message_envelope) {
+	message* m;
+	pcb* receiving_proc;
 	
-	/*
-	atomic ( on ) ;
-	set sender_procid , destination_procid ;
-	pcb_t * receiving_proc = get_pcb_from_pid ( receiving_pid ) ;
-	enqueue env onto the msg_queue of receiving_proc ;
-	if ( receiving_proc->state is BLOCKED_ON_RECEIVE ) {
-		set receiving_proc state to ready ;
-		rpq_enqueue ( receiving_proc ) ;
+	__disable_irq(); //atomic(on);
+	
+	//create message package
+	m = message_new(get_procid_of_current_process(), destination_id, (msgbuf *)message_envelope);
+	receiving_proc = get_pcb_pointer_from_process_id(destination_id);
+	m_enqueue(&g_message_queue, m);
+
+	//check if receiver is waiting
+	if (receiving_proc->m_state == BLOCKED_ON_RECEIVE) {
+		receiving_proc->m_state = READY;
+		remove_queue_node(&g_blocked_on_receive_queue, receiving_proc);
+		enqueue(&g_ready_queue, receiving_proc);
 	}
-	atomic ( off ) ;
-	*/
-	
+
+	__enable_irq();
+	return RTX_OK; //todo: is this what we want?
 }
 
 void *k_receive_message(int *sender_id) {
@@ -27,6 +36,14 @@ void *k_receive_message(int *sender_id) {
 	atomic ( off ) ;
 	return env ;
 	*/
+	int current_process = get_procid_of_current_process();
+	__disable_irq();
+	while (!m_any_messages(g_message_queue, current_process)){//} current_process msg_queue is empty ) {
+		get_pcb_pointer_from_process_id(current_process)->m_state = BLOCKED_ON_RECEIVE ;
+		release_processor() ;
+	}
+	__enable_irq();
+	return RTX_OK; //todo: is this what we want?
 }
 
 /*
@@ -39,5 +56,86 @@ int k_delayed_send(int process_id, void *message_envelope, int delay) {
 	if (delay < 0) {
 		return RTX_ERR;
 	}
-	
+	return RTX_OK; //todo: is this what we want?
+}
+
+// ------------QUEUE FUNCTIONS ------------------------------------------------
+
+//Add the node to the tail end of the queue
+void m_enqueue(message** targetQueue, message* element) {
+	message* queue = *targetQueue;
+
+	//check if empty
+	if (m_is_empty(*targetQueue)) {
+		*targetQueue = element;
+		element->mp_next = NULL;
+		return;
+	}
+
+	//compare to first item in queue
+	if (element->destination_id < (*targetQueue)->destination_id){
+		element->mp_next = *targetQueue;
+		*targetQueue = element;
+		return;
+	}
+
+	//iterate through to find where to insert
+	while (queue->mp_next != NULL && element->destination_id >= queue->mp_next->destination_id) {
+		queue = queue->mp_next;
+	}
+
+	//insert
+	element->mp_next = queue->mp_next;
+	queue->mp_next = element;
+}
+
+//Remove and return a node from the front end of the queue
+message* m_dequeue(message** targetQueue) {
+	if (!m_is_empty(*targetQueue)) {
+		message* element = *targetQueue;
+		*targetQueue = (*targetQueue)->mp_next;
+		element->mp_next = NULL;
+		return element;
+	}
+	return NULL; //null if nothing to dequeue
+}
+
+//Removes a node from the queue, regardless of its position
+//The input parameter asks for the contents and not for the queue_node
+void m_remove_queue_node(message** targetQueue, message* element) {
+	message* queue = *targetQueue;
+
+	//compare to first item in queue
+	if (*targetQueue == element){
+		*targetQueue = (*targetQueue)->mp_next;
+	}
+
+	//iterate through to find what to remove
+	while (queue->mp_next != element) {
+		queue = queue->mp_next;
+	}
+
+	//remove
+	queue->mp_next = element->mp_next;
+	element->mp_next = NULL;
+}
+
+//Emptiness check.
+//Will return 1 if empty and 0 if not.
+U32 m_is_empty(message* targetQueue) {
+	if (targetQueue == NULL) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+//check if any messages with destination id.
+//Will return 1 if empty and 0 if not.
+U32 m_any_messages(message* targetQueue, int destination_id) {
+	if (targetQueue == NULL) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
--- src/k_ipc.h
+++ src/k_ipc.h
@@ -2,16 +2,46 @@
 #define K_IPC_H
 
 #include "rtx.h"
+#include "k_rtx.h"
+#include "k_process.h"
 
 #define DEFAULT 0 /* A general purpose message.*/
 #define KCD_REG 1 /* A message to register a command with the Keyboard Command Decoder Process */
 
-struct msgbuf {
+//their required message format
+typedef struct msgbuf {
 	int mtype; /* user defined message type. One of the two constants above. */
 	char mtext[1]; /* body of the message */
-};
+} msgbuf;
 
-extern int k_send_message(int process_id, void* message_envelope);
+//msgbuf wrapper containing the rest of the information we need for message passing
+typedef struct message {
+	msgbuf* message_envelope;
+	int sender_id;
+	int destination_id;
+	struct message* mp_next;
+} message;
+
+message* message_new(int sender, int destination, msgbuf* envelope) {
+	message* m;
+	m->sender_id = sender;
+	m->destination_id = destination;
+	m->message_envelope = envelope;
+	m->mp_next = NULL;
+	return m;
+}
+
+/* -------- QUEUES FOR MESSAGES -------- */
+
+void m_enqueue(message** targetQueue, message* element);
+message* m_dequeue(message** targetQueue);
+void m_remove_queue_node(message** targetQueue, message* element);
+U32 m_is_empty(message* targetQueue);
+U32 m_any_messages(message* targetQueue, int destination_id);
+
+/* ----- Functions ----- */
+
+extern int k_send_message(int destination_id, void* message_envelope);
 #define send_message(pid, env) _send_message((U32)k_send_message, pid, env)
 extern int _send_message(U32 p_func, int pid, void* env) __SVC_0;
 																												
